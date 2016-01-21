c1e3103434d6c5cde1ebca2c4e947e37a8642fb3
Chris Mark <chdmark@gmail.com>
2016-01-20 17:56:27 -0800

    Create model relationships

--- app/models/qa_session.rb
+++ app/models/qa_session.rb
@@ -1,2 +1,4 @@
 class QaSession < ActiveRecord::Base
+	belongs_to :host, class_name: 'Athlete'
+	belongs_to :fan
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+	has_one :answer
+	belongs_to :fan
 end
