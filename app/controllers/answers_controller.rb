3b779a8c2291591fd6359aafc15e8b319527e722
Chris Mark <chdmark@gmail.com>
2016-01-21 22:16:23 -0800

    Save question id params

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
 class AnswersController < ApplicationController
 	def create
 		p params
-		@answer = Answer.new(qa_id: params[:qa_id], content: params[:content], image_url: params[:image_url], name: params[:name])
+		@answer = Answer.new(question_id: params[:question_id], qa_id: params[:qa_id], content: params[:content], image_url: params[:image_url], name: params[:name])
 		@answer.save
 
 		render json: @answer
