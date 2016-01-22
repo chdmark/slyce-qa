2bc00caf358c81c3ece24ed7fd39a6768fddf3c9
Chris Mark <chdmark@gmail.com>
2016-01-21 18:06:10 -0800

    activerecord first or create does not work

--- app/controllers/qas_controller.rb
+++ app/controllers/qas_controller.rb
@@ -1,10 +1,11 @@
 class QasController < ApplicationController
-	def index
-		@qa = Qa.all
-	end
+	
 
 	def create
-		athlete = Athlete.find_by(name: params[:host_name]).first_or_create(name: params[:host_name])
+		p "*" * 50
+		p params
+		p "*" * 50
+		athlete = Athlete.find_or_create_by(name: params[:host_name])
 		host_id = athlete.id
 
 		qa = Qa.new(host_id: host_id, start_time: params[:start_time], end_time: params[:end_time])
