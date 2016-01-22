b4c23170da78d49ec3bc5b8abb838c857927881e
Chris Mark <chdmark@gmail.com>
2016-01-21 22:43:13 -0800

    Add readme instructions and clean up controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,15 +4,12 @@ class QuestionsController < ApplicationController
 		@questions = Question.where(qa_id: @qa.id)
 		@answers = Answer.where(qa_id: @qa.id)
 		@unanswered_questions = Answer.where(qa_id: @qa.id, content: nil)
-
 		render json: {questions: @questions, answers: @answers, unanswered_questions: @unanswered_questions}
 	end
 
 	def create
 		@question = Question.new(qa_id: params[:qa_id], content: params[:content], name: params[:name])
-
 		@question.save
-
 		render json: @question
 	end
 end
