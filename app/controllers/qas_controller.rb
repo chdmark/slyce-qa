28f9a0053e247197e1ab9b5755e955a7f244bc4a
Chris Mark <chdmark@gmail.com>
2016-01-21 19:27:21 -0800

    Get and Post route for qas are successful. Javascript expects json back so render json in controller

--- app/controllers/qas_controller.rb
+++ app/controllers/qas_controller.rb
@@ -8,15 +8,19 @@ class QasController < ApplicationController
 		athlete = Athlete.find_or_create_by(name: params[:host_name])
 		host_id = athlete.id
 
-		qa = Qa.new(host_id: host_id, start_time: params[:start_time], end_time: params[:end_time])
+		@qa = Qa.new(host_id: host_id, start_time: params[:start_time], end_time: params[:end_time])
 
-		qa.save 
+		@qa.save 
 
-		
+		render json: @qa
+
+
 
 	end
 
 	def show
+		@qa = Qa.find(params[:id])
 		
+		render json: @qa
 	end
 end
